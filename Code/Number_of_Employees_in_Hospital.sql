8fe8169ce3e57d12e4a996228e1b4078cedc2fa7
joshichinmay44 <56485281+joshichinmay44@users.noreply.github.com>
2025-01-19 15:46:02 +0530

    Create Number_of_Employees_in_Hospital.sql

---
+++ Code/Number_of_Employees_in_Hospital.sql
@@ -0,0 +1,21 @@
+ --find number of employees inside the hospital.
+--https://www.youtube.com/watch?v=oGYinDMDfnA&list=PLBTZqjSKn0IfuIqbMIqzS-waofsPHMS0E&index=4
+
+create table hospital ( emp_id int
+, action varchar(10)
+, time datetime);
+
+insert into hospital values ('1', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('1', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:00:00');
+insert into hospital values ('2', 'out', '2019-12-22 09:15:00');
+insert into hospital values ('2', 'in', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:00:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:15:00');
+insert into hospital values ('3', 'out', '2019-12-22 09:30:00');
+insert into hospital values ('3', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('4', 'in', '2019-12-22 09:45:00');
+insert into hospital values ('5', 'out', '2019-12-22 09:40:00');
+
+with ranked_data as (select row_number() over(partition by emp_id order by time desc) as rn,* from hospital)
+select emp_id, action, time from ranked_data where rn=1 and action='in'
